bd60705a562a788ded7dbaa999ae72ef221dfa01
GiulianiViviana <giuliani.viviana@iisgalvanimi.edu.it>
2020-04-08 07:42:47 +0000

    Sistemato tasto back - Esercizio 01.1

--- first-routed-app/src/app/track/track.component.ts
+++ first-routed-app/src/app/track/track.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router, ParamMap } from '@angular/router';
 import { SpotifyService } from '../spotify.service';
 import { Observable } from 'rxjs';
-
+import { Location } from '@angular/common';
 @Component({
   selector: 'app-track',
   templateUrl: './track.component.html',
@@ -15,7 +15,7 @@ export class TrackComponent implements OnInit {
   routeObs: Observable<ParamMap>;
   track : any; //Qui salverò la traccia selezionata
   spotifyServiceObs: Observable<Object>;
-  location: any;
+
 
 
   //Usiamo la dependency injection per farci mandare i moduli del routing e dello
@@ -23,7 +23,8 @@ export class TrackComponent implements OnInit {
   constructor(
     private route: ActivatedRoute,
     private router: Router,
-    private service: SpotifyService ) { }
+    private service: SpotifyService,
+    private location: Location ) { }
 
 
   ngOnInit(): void {
