f6316b5e3da53078f8846d246d70eaefb50181fa
GiulianiViviana <giuliani.viviana@iisgalvanimi.edu.it>
2020-04-08 06:42:49 +0000

    Inizio 01.1

--- first-routed-app/src/app/spotify.service.ts
+++ first-routed-app/src/app/spotify.service.ts
@@ -15,7 +15,7 @@ export class SpotifyService {
     const url = `https://api.spotify.com/v1/search?q=${query}&type=track`;
     const headers = new HttpHeaders({
       Authorization:
-        'Bearer BQBa-BbByJIS2ryO26pkn0WZFRSwbPrGy0l2Bauz7OBP0coHKCW40OXSqev-0u4komUbFNPJ3h9Bkis0MV9rYJqImQuNmExRHNgjIWR-dj83ZjlioEUVnGqL9oYvgubUUUS-F_kuwhEhuRqQP7uOEhpuZiFsn_mFPaw'
+        'Bearer BQAj2TmQ7essJ7zP7h9DQkeObH5kh2MPJhXUeRF6NLcrOACSyv6oyvYpsDwcuCZ17JcQf8MAvkxa5dg4g7goi4VjXUoUeNS3gX45Gg-APydf-MtU60zqQYL2hrJWk7fPFPcgSjH2Bh-GtqJt1U7ki1euni0GeSnCsyk'
     });
 
     let obsTracks = this.http.get(url, { headers });
